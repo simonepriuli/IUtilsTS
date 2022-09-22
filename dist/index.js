e40048aeb4c90f5815f914d0ddbd9c72fb2914cf
2dayevents <2dayeventsapp@gmail.com>
2022-09-22 18:47:25 +0200

    ok

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,2 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const IEnumerable_1 = require("./IEnumerable");
-const usr1 = { name: 'simone', age: 22 };
-const usr2 = { name: 'john', age: 30 };
-let Users = new IEnumerable_1.IEnumerable;
-Users.Append(usr1);
-Users.Append(5);
-Users.Append(usr2);
-Users.Append(12);
-Users.Append('Hello World!');
-Users.Append({ name: 'marco', age: 25 });
-Users.Append(-4);
-console.log(Users.NumbersToArray());
--- index.ts
+++ index.ts
@@ -1,23 +1,3 @@
 import { IEnumerable } from './IEnumerable';
 
-interface IUser {
-  name: string,
-  age: number
-}
 
-const usr1: IUser = {name: 'simone', age: 22};
-const usr2: IUser = {name: 'john', age: 30};
-
-let Users = new IEnumerable;
-
-
-
-Users.Append(usr1);
-Users.Append(5);
-Users.Append(usr2);
-Users.Append(12);
-Users.Append('Hello World!');
-Users.Append({name: 'marco', age: 25});
-Users.Append(-4);
-
-console.log(Users.NumbersToArray());
