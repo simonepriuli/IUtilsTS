e7abf0184944dde0a2e55e9e1c3cf0228e01fc2a
Simone Priuli <simone.priuli@icloud.com>
2022-09-22 16:18:08 +0200

    compareKeys for OBJ

--- IEnumerable.ts
+++ IEnumerable.ts
@@ -1,50 +1,98 @@
 export class IEnumerable {
 
-  Enumerables = new Map();
-  id: number = -1;
+  private Enumerables = new Map();
+  private id: number = -1;
+
+  private function compareKeys(a: any, b: any) {
+    var aKeys = Object.keys(a).sort();
+    var bKeys = Object.keys(b).sort();
+    return JSON.stringify(aKeys) === JSON.stringify(bKeys);
+  }
 
   //Aggiunge un oggetto any in coda
   Append(obj: any) {
-    if (this.id == -1) {this.id = 0}
+    if (this.id == -1) { this.id = 0 }
     this.Enumerables.set(this.id, obj);
     this.id += 1;
   }
 
   //Controlla se l'IEnumerable è vuoto
   Any() {
-    if (this.id == -1) {return false}
-    else {return true}
+    if (this.id == -1) { return false }
+    else { return true }
   }
 
   //Restituisce un Enumerator iterabile
   GetEnumerator(): Map<any, any> {
     return this.Enumerables;
-  }  
+  }
 
   //Restituisce la dimensione dell'IEnumerable
   Size(): number {
     let x: number = 0;
-    this.Enumerables.forEach(()=> {
-      x = x +1;
+    this.Enumerables.forEach(() => {
+      x = x + 1;
     });
     return x;
-  } 
+  }
 
   //Restituisce la media tra i numeri contenuti all'interno
   Average(): number {
     let sampleValue: number = 0;
-    
+
     let average: number = 0;
     let aveCount: number = 0;
-    
-    this.Enumerables.forEach((value)=> {
-      if (typeof(value) == typeof(sampleValue)) {
+
+    this.Enumerables.forEach((value) => {
+      if (typeof (value) == typeof (sampleValue)) {
         average += value;
         aveCount += 1;
       }
     })
-    if (aveCount != 0) {return average/aveCount}
-    else {return 0}
+    if (aveCount != 0) { return average / aveCount }
+    else { return 0 }
+  }
+
+  //Restituisce il massimo tra i numeri contenuti all'interno
+  Max(): number {
+    let sampleValue: number = 0;
+    let x: number = this.Enumerables.get(0);
+
+    this.Enumerables.forEach((value) => {
+      if (typeof (value) == typeof (sampleValue)) {
+        if (value > x) { x = value }
+      }
+    })
+
+    return x;
+  }
+
+  //Restituisce il minimo tra i numeri contenuti all'interno
+  Min(): number {
+    let sampleValue: number = 0;
+    let x: number = this.Enumerables.get(0);
+
+    this.Enumerables.forEach((value) => {
+      if (typeof (value) == typeof (sampleValue)) {
+        if (value < x) { x = value }
+      }
+    })
+
+    return x;
+  }
+
+  //Restituisce il minimo tra i numeri contenuti all'interno
+  Sum(): number {
+    let sampleValue: number = 0;
+    let x: number = 0;
+
+    this.Enumerables.forEach((value) => {
+      if (typeof (value) == typeof (sampleValue)) {
+        x += value;
+      }
+    })
+
+    return x;
   }
 
   //restituisce un arry contenente tutti i number nell'IEnumerable
@@ -53,9 +101,9 @@ export class IEnumerable {
     let array: any[] = [];
 
     this.Enumerables.forEach((value) => {
-      if (typeof(value) == typeof(sampleObj)) {array.push(value)}
+      if (typeof (value) == typeof (sampleObj)) { array.push(value) }
     });
-    
+
     return array;
   }
 
@@ -65,9 +113,9 @@ export class IEnumerable {
     let array: any[] = [];
 
     this.Enumerables.forEach((value) => {
-      if (typeof(value) == typeof(sampleObj)) {array.push(value)}
+      if (typeof (value) == typeof (sampleObj)) { array.push(value) }
     });
-    
+
     return array;
   }
 
@@ -77,11 +125,71 @@ export class IEnumerable {
     let array: any[] = [];
 
     this.Enumerables.forEach((value) => {
-      if (typeof(value) == typeof(sampleObj)) {array.push(value)}
+      if (this.compareKeys(value, sampleObj)) { array.push(value) }
     });
-    
+
     return array;
   }
-  
-  
+
+  //restituisce il primo elemento dell'IEnumerable
+  GetFirst() {
+    return this.Enumerables.get(0);
+  }
+
+  //restituisce l'ultimo elemento dell'IEnumerable
+  GetLast() {
+    return this.Enumerables.get(this.id - 1);
+  }
+
+  //restiutisce il conteggio dei valori number all'interno dell'IEnumerable
+  NumberCount() {
+    let sampleObj: number = 0;
+    let count: number = 0;
+
+    this.Enumerables.forEach((value) => {
+      if (typeof (value) == typeof (sampleObj)) { count += 1 }
+    });
+
+    return count;
+  }
+  //restiutisce il conteggio dei valori string all'interno dell'IEnumerable
+  StringCount() {
+    let sampleObj: string = '';
+    let count: number = 0;
+
+    this.Enumerables.forEach((value) => {
+      if (typeof (value) == typeof (sampleObj)) { count += 1 }
+    });
+
+    return count;
+  }
+
+  //restiutisce il conteggio dei valori object all'interno dell'IEnumerable
+  ObjCount() {
+    let sampleObj: object = {};
+    let count: number = 0;
+
+    this.Enumerables.forEach((value) => {
+      if (typeof (value) == typeof (sampleObj)) { count += 1 }
+    });
+
+    return count;
+  }
+
+  GreaterThan(x: number): boolean {
+    let check: boolean = true;
+    this.Enumerables.forEach((a) => {
+      if (a <= x) { check = false }
+    });
+    return check;
+  }
+
+  LowerThan(x: number): boolean {
+    let check: boolean = true;
+    this.Enumerables.forEach((a) => {
+      if (a >= x) { check = false }
+    });
+    return check;
+  }
+
 }
