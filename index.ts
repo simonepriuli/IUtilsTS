db07f412eb2bf5fa9f2ef4bf91768e9759168ba9
Simone Priuli <simone.priuli@icloud.com>
2022-09-22 16:18:34 +0200

    comparekeys test

--- index.ts
+++ index.ts
@@ -1,2 +1,26 @@
 import { IEnumerable } from './IEnumerable';
 
+//IEnumerable deriva da C# 
+
+interface IUser {
+  username: string,
+  password: string
+}
+
+const usr1: IUser = {username: 'simonepriuli', password: 'abc123'};
+const usr2: IUser = {username: 'jondoe', password: 'password!'};
+
+//array, map, obj, tuple
+
+let Users = new IEnumerable;
+
+
+
+
+const obj1 = {name: 'simone'};
+const obj2 = {names: 'giorgio'};
+
+Users.Append(obj1);
+Users.Append(obj2);
+
+console.log(Users.ObjToArray(obj1));
