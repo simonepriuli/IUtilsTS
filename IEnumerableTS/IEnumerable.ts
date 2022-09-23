a51597129c5fbe39763bce784531aa67f1c67dcc
2dayevents <2dayeventsapp@gmail.com>
2022-09-23 17:26:52 +0200

    JSdoc implementation

---
+++ IEnumerableTS/IEnumerable.ts
@@ -0,0 +1,305 @@
+export class IEnumerable {
+  private Enumerables = new Map();
+  private id: number = -1;
+  private IEnumerableType: any = {};
+
+  /**
+   * IEnumerable constructor.
+   * @param {any} IEnumerableType - IEnumerable take a sample object IEnumerableType to instruct the Append method to only accept that type.
+   */
+  constructor(IEnumerableType?: any) {
+    this.IEnumerableType = IEnumerableType;
+  }
+
+  private wrongTypeErr(obj1: any): never {
+    throw new Error(
+      "Can't .Append() object of type: " +
+        typeof obj1 +
+        " on IEnumerable of type: " +
+        typeof this.IEnumerableType
+    );
+  }
+
+  private compareKeys = (a: any, b: any) => {
+    var aKeys = Object.keys(a).sort();
+    var bKeys = Object.keys(b).sort();
+    return JSON.stringify(aKeys) === JSON.stringify(bKeys);
+  };
+
+  private firstNumber = () => {
+    this.Enumerables.forEach((a) => {
+      if (typeof a == typeof this.id) {
+        return a;
+      } else {
+        return 0;
+      }
+    });
+  };
+
+  /**
+   * Append the parameter at the end of the IEnumerable
+   * @param {any} any - Any if you did not set the constructor IEnumerableType.
+   */
+  Append(obj: any) {
+    if (this.IEnumerableType == undefined) {
+      if (this.id == -1) {
+        this.id = 0;
+      }
+      this.Enumerables.set(this.id, obj);
+      this.id += 1;
+    } else {
+      if (this.compareKeys(obj, this.IEnumerableType)) {
+        if (this.id == -1) {
+          this.id = 0;
+        }
+        this.Enumerables.set(this.id, obj);
+        this.id += 1;
+      } else {
+        this.wrongTypeErr(obj);
+      }
+    }
+  }
+
+  /**
+   * Returns true if the IEnumerable contains any object.
+   * @return {Boolean} boolean.
+   */
+  Any() {
+    if (this.id == -1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  /**
+   * Returns an iterable Map.
+   * @return {Map} Map.
+   */
+  GetEnumerator(): Map<any, any> {
+    return this.Enumerables;
+  }
+
+  /**
+   * Returns the size of the IEnumerable.
+   * @return {Number} Number.
+   */
+  Size(): number {
+    let x: number = 0;
+    this.Enumerables.forEach(() => {
+      x = x + 1;
+    });
+    return x;
+  }
+
+  /**
+   * Returns the average of the numbers inside the IEnumerable.
+   * @return {Number} Number.
+   */
+  Average(): number {
+    let sampleValue: number = 0;
+
+    let average: number = 0;
+    let aveCount: number = 0;
+
+    this.Enumerables.forEach((value) => {
+      if (typeof value == typeof sampleValue) {
+        average += value;
+        aveCount += 1;
+      }
+    });
+    if (aveCount != 0) {
+      return average / aveCount;
+    } else {
+      return 0;
+    }
+  }
+
+  //Restituisce il massimo tra i numeri contenuti all'interno
+  /* 
+  Max() {
+    let sampleValue: number = 0;
+    let x: any = this.firstNumber();
+
+    this.Enumerables.forEach((value) => {
+      if (typeof (value) == typeof (sampleValue)) {
+        if (value > x) { x = value }
+      }
+    })
+
+    return x;
+  }
+  */
+
+  //Restituisce il minimo tra i numeri contenuti all'interno
+  /*
+  Min() {
+    let sampleValue: number = 0;
+    let x: any = this.firstNumber();
+
+    this.Enumerables.forEach((value) => {
+      if (typeof (value) == typeof (sampleValue)) {
+        if (value < x) { x = value }
+      }
+    })
+
+    return x;
+  }
+  */
+
+  /**
+   * Returns the sum of the numbers inside the IEnumerable.
+   * @return {Number} Number.
+   */
+  Sum(): number {
+    let sampleValue: number = 0;
+    let x: number = 0;
+
+    this.Enumerables.forEach((value) => {
+      if (typeof value == typeof sampleValue) {
+        x += value;
+      }
+    });
+
+    return x;
+  }
+
+  /**
+   * Returns an array with the numbers inside the IEnumerable.
+   * @return {Array} Array.
+   */
+  NumbersToArray() {
+    let sampleObj: number = 0;
+    let array: any[] = [];
+
+    this.Enumerables.forEach((value) => {
+      if (typeof value == typeof sampleObj) {
+        array.push(value);
+      }
+    });
+
+    return array;
+  }
+
+  /**
+   * Returns an array with the strings inside the IEnumerable.
+   * @return {Array} Array.
+   */
+  StringsToArray() {
+    let sampleObj: string = "";
+    let array: any[] = [];
+
+    this.Enumerables.forEach((value) => {
+      if (typeof value == typeof sampleObj) {
+        array.push(value);
+      }
+    });
+
+    return array;
+  }
+
+  /**
+   * Returns an array with the objects inside the IEnumerable.
+   * @param {any} any - Specify the object type to aggregate.
+   * @return {Array} Array.
+   */
+  ObjToArray(sampleObj: any) {
+    let array: any[] = [];
+
+    this.Enumerables.forEach((value) => {
+      if (this.compareKeys(value, sampleObj)) {
+        array.push(value);
+      }
+    });
+
+    return array;
+  }
+
+  /**
+   * Returns the first element of the IEnumerable.
+   * @return {Any} Any.
+   */
+  GetFirst() {
+    return this.Enumerables.get(0);
+  }
+
+  /**
+   * Returns the last element of the IEnumerable.
+   * @return {Any} Any.
+   */
+  GetLast() {
+    return this.Enumerables.get(this.id - 1);
+  }
+
+  /**
+   * Returns the count of numbers inside IEnumerable.
+   * @return {Number} Number.
+   */
+
+  NumberCount() {
+    let sampleObj: number = 0;
+    let count: number = 0;
+
+    this.Enumerables.forEach((value) => {
+      if (typeof value == typeof sampleObj) {
+        count += 1;
+      }
+    });
+
+    return count;
+  }
+
+  /**
+   * Returns the count of strings inside IEnumerable.
+   * @return {Number} Number.
+   */
+  StringCount() {
+    let sampleObj: string = "";
+    let count: number = 0;
+
+    this.Enumerables.forEach((value) => {
+      if (typeof value == typeof sampleObj) {
+        count += 1;
+      }
+    });
+
+    return count;
+  }
+
+  /**
+   * Returns the count of objects inside IEnumerable.
+   * @param {any} Any - Specify the object type to count.
+   * @return {Number} Number.
+   */
+  ObjCount(sampleObj: any) {
+    let count: number = 0;
+
+    this.Enumerables.forEach((value) => {
+      if (this.compareKeys(value, sampleObj)) {
+        count += 1;
+      }
+    });
+
+    return count;
+  }
+
+  GreaterThan(x: number): boolean {
+    let check: boolean = true;
+    this.Enumerables.forEach((a) => {
+      if (a <= x) {
+        check = false;
+      }
+    });
+    return check;
+  }
+
+  LowerThan(x: number): boolean {
+    let check: boolean = true;
+    this.Enumerables.forEach((a) => {
+      if (a >= x) {
+        check = false;
+      }
+    });
+    return check;
+  }
+}
--- index.ts
+++ index.ts
@@ -1,3 +1,20 @@
-import { IEnumerable } from './IEnumerable';
+import { IEnumerable } from './IEnumerableTS/IEnumerable'
 
+interface IUser {
+    username: string,
+    age: number
+}
+
+const usr1: IUser = {username: 'simone', age: 22};
+const usr2: IUser = {username: 'john', age: 30};
+
+let Users = new IEnumerable;
+
+Users.Append(usr1);
+Users.Append(1);
+Users.Append('Ciao');
+Users.Append(usr2);
+Users.Append(10);
+
+console.log(Users.ObjCount(usr1)); 
 
